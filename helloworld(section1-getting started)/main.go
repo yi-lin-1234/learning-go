732bc94309aa8bea53a5580a9831df703dc06699
yilin <james.lin1065@gmail.com>
2023-03-20 22:51:47 -0400

    section 1

---
+++ helloworld(section1-getting started)/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+//google doc
+// https://docs.google.com/document/d/1PaOISyrJqxtHi9m7WNvdIj7yvHiNfwEOFSXMAs9jtrE/edit
+
+// how do we run the code in our project?
+// go run main.
+
+// what does package main mean?
+// package main => executable package, other name => reusable package
